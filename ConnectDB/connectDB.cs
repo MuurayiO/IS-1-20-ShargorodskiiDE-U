3187e3d3f5c01c25ef20287bfa000345b7f6698c
MuurayiO <105793137+MuurayiO@users.noreply.github.com>
2022-12-05 18:41:30 +0500

    ап

---
+++ ConnectDB/connectDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConnectDB
+{
+    public class connectDB
+    {
+        string strConn;
+        MySqlConnection conn;
+
+        public MySqlConnection Connection()
+        {
+            conn = new MySqlConnection(strConn);
+            return conn;
+        }
+        public string ReturnConn()
+        {
+            return strConn;
+
+        }
+        public connectDB(string connect)
+        {
+            strConn = connect;
+        }
+    }
+}
