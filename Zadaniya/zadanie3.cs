b386e6f8bc43cc4ab89ebf93c37a0b3e3b95c875
MuurayiO <105793137+MuurayiO@users.noreply.github.com>
2022-12-05 19:34:22 +0500

    ВСЕ ГОТОВО

--- Zadaniya/zadanie3.cs
+++ Zadaniya/zadanie3.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Guna.UI2.WinForms;
 using MySql.Data.MySqlClient;
 
 namespace IS_1_20_ShargorodskiiDE_U
